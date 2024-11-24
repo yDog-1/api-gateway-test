b5cdbb36980b4a03a017f6ddd468e077e5398983
yDog <ydog-1@outlook.jp>
2024-11-24 22:06:22 +0900

    init Terraformを初期化

---
+++ terraform/terraform.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "s3" {
+    key                         = "api-gateway-test/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    use_lockfile                = true # State Lockを有効化
+  }
+  required_version = "~> 1.10"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      type = "learn"
+    }
+  }
+}
+
+locals {
+  name_prifix = "api-gateway-test"
+}
