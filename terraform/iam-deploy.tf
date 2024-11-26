30b8f77a0a7065be18bb4fca0d27e92d7294a79e
yDog <ydog-1@outlook.jp>
2024-11-26 21:59:45 +0900

    リファクタ コメントを追加

--- terraform/iam-deploy.tf
+++ terraform/iam-deploy.tf
@@ -2,12 +2,15 @@ locals {
   github_oidc_provider_arn = "arn:aws:iam::${local.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
 }
 
+# GitHub ActionsからECRにイメージをプッシュするためのIAMポリシードキュメント
 data "aws_iam_policy_document" "ecr_push_assume" {
   version = "2012-10-17"
   statement {
+    # GitHub ActionsによるAssume Roleの許可
     effect  = "Allow"
     actions = ["sts:AssumeRoleWithWebIdentity"]
     principals {
+      # OIDCプロバイダーの指定
       type        = "Federated"
       identifiers = [local.github_oidc_provider_arn]
     }
@@ -17,6 +20,7 @@ data "aws_iam_policy_document" "ecr_push_assume" {
       values   = ["sts.amazonaws.com"]
     }
     condition {
+      # リポジトリとブランチの指定
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values   = ["repo:${var.git_author}/${var.git_repo}:ref/heads/main"]
@@ -24,6 +28,7 @@ data "aws_iam_policy_document" "ecr_push_assume" {
   }
 }
 
+# ECRへのプッシュを許可するIAMポリシードキュメント
 data "aws_iam_policy_document" "ecr_push_policy" {
   version = "2012-10-17"
   statement {
@@ -40,16 +45,15 @@ data "aws_iam_policy_document" "ecr_push_policy" {
   }
 }
 
+# GitHub ActionsからECRにイメージをプッシュするためのIAMロール
 resource "aws_iam_role" "ecr_push" {
   name               = "${local.project_name_prefix}_ecr_push_role"
   assume_role_policy = data.aws_iam_policy_document.ecr_push_assume.json
 }
-
 resource "aws_iam_policy" "ecr_push_policy" {
   name   = "${local.project_name_prefix}_ecr_push_policy"
   policy = data.aws_iam_policy_document.ecr_push_policy.json
 }
-
 resource "aws_iam_role_policy_attachment" "ecr_push" {
   role       = aws_iam_role.ecr_push.name
   policy_arn = aws_iam_policy.ecr_push_policy.arn
