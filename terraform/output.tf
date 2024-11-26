6ba677ab77aad153dc1510a64c4ae7f8797863e6
yDog <ydog-1@outlook.jp>
2024-11-26 21:59:26 +0900

    修正 ECRのURLでなく、名前をアウトプットする

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,11 @@
 output "frontend_ecr" {
-  description = "フロントエンド用のECRリポジトリURL"
-  value       = aws_ecr_repository.frontend.repository_url
-  sensitive   = true
+  description = "フロントエンド用のECRリポジトリ名"
+  value       = aws_ecr_repository.frontend.name
 }
 
 output "backend_ecr" {
+  description = "バックエンド用のECRリポジトリ名"
+  value       = aws_ecr_repository.backend.name
 }
 
 output "ecr_push_role_arn" {
