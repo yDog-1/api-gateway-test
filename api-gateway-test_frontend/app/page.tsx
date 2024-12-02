1708b6931b5d2dabbf50693dbddbef8ada1d6220
yDog <ydog-1@outlook.jp>
2024-12-02 21:08:04 +0900

    rm Next.jsのロゴを削除

--- api-gateway-test_frontend/app/page.tsx
+++ api-gateway-test_frontend/app/page.tsx
@@ -13,14 +13,6 @@ export default function Home() {
 						Welcome page
 					</Link>
 				</div>
-				<Image
-					className="dark:invert"
-					src="/next.svg"
-					alt="Next.js logo"
-					width={180}
-					height={38}
-					priority
-				/>
 				<ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
 					<li className="mb-2">
 						Get started by editing{" "}
